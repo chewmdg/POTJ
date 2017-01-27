f2febf4dc61a71822e000fa09cfea066f1a97127
nwg5817 <nwg5817@gmail.com>
2017-01-27 03:26:59 -0500

    Revert "make the change"
    This reverts commit 75825c41f26f71cf014b66ce15f9810c28845f44.

--- Path of the Jedi/Source/PathOfTheJedi/Defs/DamageDef_CR.cs
+++ Path of the Jedi/Source/PathOfTheJedi/Defs/DamageDef_CR.cs
@@ -1,4 +1,10 @@
-﻿using Verse;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
 
 namespace PathOfTheJedi
 {

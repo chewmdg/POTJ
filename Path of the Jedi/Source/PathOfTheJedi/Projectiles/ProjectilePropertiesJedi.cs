23449ff581d3925ccf6523e2e4709432dfd5031f
nwg5817 <nwg5817@gmail.com>
2017-01-27 03:27:21 -0500

    Revert "Revert "make the change""
    This reverts commit f2febf4dc61a71822e000fa09cfea066f1a97127.

---
+++ Path of the Jedi/Source/PathOfTheJedi/Projectiles/ProjectilePropertiesJedi.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+
+namespace PathOfTheJedi
+{
+    public class ProjectilePropertiesJedi : ProjectileProperties
+    {
+        public float armorPenetration = 0;
+        public int pelletCount = 1;
+        public float spreadMult = 1;
+        public bool damageAdjacentTiles = false;
+        public bool dropsCasings = false;
+        public string casingMoteDefname = "Mote_EmptyCasing";
+    }
+}
